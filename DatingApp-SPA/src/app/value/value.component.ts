1560d120a5ede4aa86d3ea9b978ca3534564b0a7
Ivory <magiadiaria@yahoo.com>
2020-05-13 23:11:31 -0400

    Login validation 1

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -15,12 +15,12 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
-  getValues(){
+  getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
-    }, error =>{
+    }, error => {
       console.log(error);
-    })
+    });
   }
 
 }
